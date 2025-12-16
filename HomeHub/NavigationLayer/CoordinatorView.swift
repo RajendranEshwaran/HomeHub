286d97425d2f450665f66eec6b200b2931e40a34
RAJENDRAN ESHWARAN <10699484+RajendranEshwaran@users.noreply.github.com>
2025-12-16 18:30:37 -0500

    updating the logic and code implementation
    Code implementation in coordinatorView

--- HomeHub/NavigationLayer/CoordinatorView.swift
+++ HomeHub/NavigationLayer/CoordinatorView.swift
@@ -2,6 +2,32 @@
 //  CoordinatorView.swift
 //  HomeHub
 //
-//  Created by RajayGoms on 12/16/25.
+//  Created by Rajendran Eshwaran on 12/16/25.
 //
 
+import Foundation
+import SwiftUI
+import Combine
+
+struct CoordinatorView: View {
+    @EnvironmentObject private var coordinator: Coordinator
+    var body: some View {
+        NavigationStack(path: $coordinator.navigationPath) {
+            coordinator.currentPageView(view: coordinator.rootPage)
+                .navigationDestination(for: AppPages.self) { page in
+                    coordinator.currentPageView(view: page)
+                        .environmentObject(coordinator)
+                }
+            
+                .sheet(item: $coordinator.currentSheet){ sheet in
+                    coordinator.currentSheetView(sheet: sheet)
+                        .environmentObject(coordinator)
+                }
+            
+                .fullScreenCover(item: $coordinator.currentFullCover){ cover in
+                    coordinator.currentFullCoverView(cover: cover)
+                        .environmentObject(coordinator)
+                }
+        }.environmentObject(coordinator)
+    }
+}
